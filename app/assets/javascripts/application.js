c445ec41b1d8da7354ed6077e8530ccb838787f4
Xavier <xavier.tremel@gmail.com>
2016-12-27 13:37:59 +0100

    minor improvements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-  $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
